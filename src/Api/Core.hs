9b8bfa05876ab379c85931c339cd471df2ff2bae
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-04-30 21:54:44 +0200

    Use newly created secret establishment service

--- src/Api/Core.hs
+++ src/Api/Core.hs
@@ -1,12 +1,17 @@
-{-# Language OverloadedStrings #-}
+{-# Language OverloadedStrings, TemplateHaskell #-}
 
 module Api.Core where
 
+import           Control.Lens                ( makeLenses )
 import qualified Data.ByteString.Char8 as B
 import           Snap.Core
 import           Snap.Snaplet
 
-data Api = Api
+import           Api.Services.EstablishSecret
+
+data Api = Api { _secretService :: Snaplet SecretService }
+
+makeLenses ''Api
 
 apiRoutes :: [(B.ByteString, Handler b Api ())]
 apiRoutes = [("status", method GET respondOk)]
@@ -16,5 +21,6 @@ respondOk = modifyResponse $ setResponseCode 200
 
 apiInit :: SnapletInit b Api
 apiInit = makeSnaplet "api" "Core Api" Nothing $ do
+    secretService <- nestSnaplet "secrets" secretService secretServiceInit
     addRoutes apiRoutes
-    return Api
+    return $ Api secretService
