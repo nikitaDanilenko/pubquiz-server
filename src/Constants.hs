1a0c1e265e779cf56310c3a1e0d87a8cde23020d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-06 21:28:21 +0200

    Make paths OS independent

--- src/Constants.hs
+++ src/Constants.hs
@@ -1,13 +1,25 @@
 module Constants where
 
+import Data.List       ( intercalate )
+import System.FilePath ( pathSeparator )
+
 sessionKeysFile :: String
-sessionKeysFile = "./db/sessionKeys.txt"
+sessionKeysFile = addSeparator [".", "db", "sessionKeys.txt"]
 
 userFile :: String
-userFile = "./db/users.txt"
+userFile = addSeparator [".", "db", "users.txt"]
 
 secretFile :: String
-secretFile = "./db/secrets.txt"
+secretFile = addSeparator [".", "db", "secrets.txt"]
+
+quizzesFolder :: String
+quizzesFolder = addSeparator [".", "quizzes"]
+
+addSeparator :: [String] -> String
+addSeparator = intercalate [pathSeparator]
+
+locked :: String
+locked = ".locked"
 
 publicExponent :: Integer
 publicExponent = 103787
