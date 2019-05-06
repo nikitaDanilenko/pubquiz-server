12632a336a8618c46fd2191f6ffcc31bcc6b34a1
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-06 21:44:57 +0200

    Add first part of basic functionality.

--- src/Api/Services/QuizService.hs
+++ src/Api/Services/QuizService.hs
@@ -2,24 +2,48 @@
 
 module Api.Services.QuizService where
 
+import Control.Monad                        ( filterM )
+import Control.Monad.IO.Class               ( liftIO )
 import qualified Data.ByteString.Char8 as B 
-import Snap.Core                            ( method, Method ( GET, POST ) )
+import Data.Maybe                           ( maybe )
+import Snap.Core                            ( method, Method ( GET, POST ), writeBS, modifyResponse,
+                                              setResponseCode, getPostParam )
 import Snap.Snaplet                         ( Handler )
+import System.Directory                     ( doesFileExist, getDirectoryContents )
+
+import Constants                            ( quizzesFolder, locked, addSeparator, lock )
 
 data QuizService = QuizService
 
 quizRoutes :: [(B.ByteString, Handler b QuizService ())]
 quizRoutes = [
     ("all", method GET sendAvailable),
+    ("getQuizData", method GET getSingleQuizData),
     ("update", method POST updateQuiz),
     ("lock", method POST lockQuiz)
     ]
 
+-- Finds the list of unlocked quizzes and returns it in bulk.
 sendAvailable :: Handler b QuizService ()
-sendAvailable = undefined
+sendAvailable = do
+    quizzes <- liftIO (getDirectoryContents quizzesFolder)
+    let proper = drop 2 quizzes -- Drops "." and "..". 
+    nonLockedQuizzes <- liftIO (filterM isQuizOpen proper)
+    writeBS (B.pack (unlines nonLockedQuizzes))
+    modifyResponse (setResponseCode 200)
+
+getSingleQuizData :: Handler b QuizService ()
+getSingleQuizData = undefined
 
 updateQuiz :: Handler b QuizService ()
 updateQuiz = undefined
 
 lockQuiz :: Handler b QuizService ()
-lockQuiz = undefined
+lockQuiz = do
+    quiz <- getPostParam lock
+    let act = maybe (pure ()) (\q -> writeFile (addSeparator [quizzesFolder, B.unpack q]) "") quiz
+    liftIO act
+    modifyResponse (setResponseCode 200)
+
+isQuizOpen :: String -> IO Bool
+isQuizOpen folder = doesFileExist (addSeparator [folder, locked])
