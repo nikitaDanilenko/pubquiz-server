23c3ac00aba0d407b6db3f1b352eaab55bb18ded
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-08 18:39:44 +0200

    Add functionality for posting updates

--- src/Api/Services/QuizService.hs
+++ src/Api/Services/QuizService.hs
@@ -2,19 +2,22 @@
 
 module Api.Services.QuizService where
 
+import Control.Applicative                  ( liftA2 )
 import Control.Exception                    ( catch )
 import Control.Exception.Base               ( IOException )
 import Control.Monad                        ( filterM )
 import Control.Monad.IO.Class               ( liftIO )
 import qualified Data.ByteString.Char8 as B 
+import Data.Function                        ( on )
 import Data.Maybe                           ( maybe )
 import Snap.Core                            ( method, Method ( GET, POST ), writeBS, modifyResponse,
                                               setResponseCode, getPostParam )
 import Snap.Snaplet                         ( Handler, SnapletInit, addRoutes, makeSnaplet )
 import System.Directory                     ( doesFileExist, getDirectoryContents )
+import System.Process                       ( callProcess )
 
 import Constants                            ( quizzesFolder, locked, addSeparator, lock, quiz,
-                                              roundsFile )
+                                              roundsFile, rounds, pageGenerator, prefix )
 
 data QuizService = QuizService
 
@@ -42,7 +45,27 @@ getSingleQuizData = getPostParam quiz >>= maybe (modifyResponse (setResponseCode
                               (\c -> writeBS c >> modifyResponse (setResponseCode 200))
         
 updateQuiz :: Handler b QuizService ()
-updateQuiz = undefined
+updateQuiz = do
+    mQuiz <- getPostParam quiz
+    mNewContent <- getPostParam rounds
+    let act = liftA2 (updateFile `on` B.unpack) mQuiz mNewContent
+    case act of
+        Nothing -> do writeBS (B.concat ["Malfolmed request:\n", 
+                                         quiz, "=", B.pack (show mQuiz), "\n", 
+                                         rounds, "=", B.pack (show mNewContent)])
+                      modifyResponse (setResponseCode 406)
+        Just _ -> modifyResponse (setResponseCode 200)
+
+mkKV :: String -> String -> String
+mkKV key value = concat [key, "=", value]
+
+updateFile :: String -> String -> IO ()
+updateFile quizPath content = 
+    writeFile fullQuizPath content >>
+    callProcess pageGenerator 
+                (map (\(k, v) -> mkKV (B.unpack k) v) [(prefix, quizPath), (rounds, fullQuizPath)])
+
+    where fullQuizPath = (addSeparator [quizPath, roundsFile])
 
 lockQuiz :: Handler b QuizService ()
 lockQuiz = do
@@ -63,7 +86,7 @@ isQuizOpen folder = fmap not (doesFileExist (addSeparator [quizzesFolder, folder
 
 readQuizFile :: B.ByteString -> IO (Maybe B.ByteString)
 readQuizFile quizPath = fmap Just (B.readFile filePath) `catch` handle where
-    filePath = (addSeparator [quizzesFolder, B.unpack quizPath, B.unpack roundsFile])
+    filePath = (addSeparator [quizzesFolder, B.unpack quizPath, roundsFile])
     
     handle :: IOException -> IO (Maybe B.ByteString)
     handle _ = putStrLn (filePath ++ " does not exist.") >> return Nothing
