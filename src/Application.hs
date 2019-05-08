9f2ed9e818483e7668314fd79de40ecbfe8d5226
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-08 08:02:42 +0200

    Remove unused imports

--- src/Application.hs
+++ src/Application.hs
@@ -5,9 +5,6 @@ module Application where
 import Api.Core
 import Control.Lens
 import Snap.Snaplet
-import Snap.Snaplet.Heist
-import Snap.Snaplet.Auth
-import Snap.Snaplet.Session
 
 data App = App { _api :: Snaplet Api }
 
