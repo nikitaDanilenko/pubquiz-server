f0151a064b7a5d24b00cefb8bbffd07fe01aac45
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-17 07:52:47 +0200

    Use centralised hashing function

--- src/Api/Services/SavedUser.hs
+++ src/Api/Services/SavedUser.hs
@@ -2,17 +2,38 @@
 
 module Api.Services.SavedUser where
 
-import "cryptonite" Crypto.Hash             ( Digest )
+import "cryptonite" Crypto.Hash             ( Digest, hash )
 import "cryptonite" Crypto.Hash.Algorithms  ( SHA3_512 )
-import qualified Data.ByteString.Char8 as B ( ByteString )
+import qualified Data.ByteString.Char8 as B ( ByteString, pack, concat )
+import System.Random                        ( newStdGen, randoms )
 
 type Hashed = Digest SHA3_512
 
+type UserName = B.ByteString
+type Password = B.ByteString
+type Salt = B.ByteString
+type HashValue = B.ByteString
+
 data SavedUser = Saved { 
-    userName :: B.ByteString, 
-    salt :: B.ByteString, 
-    hashValue :: B.ByteString 
+    userName :: UserName, 
+    salt :: Salt, 
+    hashValue :: HashValue
 } deriving (Show, Read)
 
 instance Eq SavedUser where
     s1 == s2 = userName s1 == userName s2
+
+mkUser :: UserName -> Password -> IO SavedUser
+mkUser user pass = do
+    gen <- newStdGen
+    let randomChars = (randoms gen) :: String
+        salt = B.pack randomChars
+        hashValue = mkHash user pass salt
+        savedUser = Saved user salt hashValue
+    return savedUser
+
+mkHashed :: UserName -> Password -> Salt -> Hashed
+mkHashed user pass salt = hash (B.concat [user, pass, salt])
+
+mkHash :: UserName -> Password -> Salt -> HashValue
+mkHash user pass salt = B.pack (show (mkHashed user pass salt))
