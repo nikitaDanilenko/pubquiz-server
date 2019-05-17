9fca794f01af0d79c6e10cd622cdb7b92fcfd125
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-17 16:51:14 +0200

    Use proper salts, rename functions, add creating functions

--- src/Api/Services/SavedUser.hs
+++ src/Api/Services/SavedUser.hs
@@ -4,8 +4,13 @@ module Api.Services.SavedUser where
 
 import "cryptonite" Crypto.Hash             ( Digest, hash )
 import "cryptonite" Crypto.Hash.Algorithms  ( SHA3_512 )
-import qualified Data.ByteString.Char8 as B ( ByteString, pack, concat )
-import System.Random                        ( newStdGen, randoms )
+import qualified Data.ByteString.Char8 as B ( ByteString, pack, concat, unpack )
+import Data.Char                            ( chr )
+import System.Directory                     ( doesFileExist )
+import System.Random                        ( newStdGen, randomRs )
+
+import Constants                            ( userFile, saltSize )
+import Utils                                ( readOrCreate )
 
 type Hashed = Digest SHA3_512
 
@@ -16,8 +21,8 @@ type HashValue = B.ByteString
 
 data SavedUser = Saved { 
     userName :: UserName, 
-    salt :: Salt, 
-    hashValue :: HashValue
+    userSalt :: Salt, 
+    userHash :: HashValue
 } deriving (Show, Read)
 
 instance Eq SavedUser where
@@ -26,11 +31,25 @@ instance Eq SavedUser where
 mkUser :: UserName -> Password -> IO SavedUser
 mkUser user pass = do
     gen <- newStdGen
-    let randomChars = (randoms gen) :: String
-        salt = B.pack randomChars
+    let randomChars = map chr (randomRs (33, 126) gen)
+        salt = B.pack (take saltSize randomChars)
         hashValue = mkHash user pass salt
         savedUser = Saved user salt hashValue
     return savedUser
+
+mkAndSaveUser :: UserName -> Password -> IO ()
+mkAndSaveUser user pass = do
+    text <- readOrCreate useFile
+    let ls = lines text
+        users = map (read :: String -> SavedUser) ls
+        exists = any (\u -> userName u == user) users
+    if exists 
+        then putStrLn (unwords ["User", B.unpack user, "already exists.", "Nothing changed."])
+        else do
+            newUser <- mkUser user pass
+            let newLs = show newUser : ls
+                newText = unlines newLs
+            writeFile userFile newText
 
 mkHashed :: UserName -> Password -> Salt -> Hashed
 mkHashed user pass salt = hash (B.concat [user, pass, salt])
