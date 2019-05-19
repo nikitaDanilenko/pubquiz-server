a1dd20ad8688cb3baf127f1a435510ae19fa00d2
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-19 13:00:41 +0200

    Move route creation to utilities

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,8 +1,20 @@
 module Utils where
 
-import System.Directory ( doesFileExist )
+import qualified Data.ByteString.Char8 as B 
+import Snap.Snaplet                         ( Handler )
+import Snap.Util.CORS                       ( applyCORS, defaultOptions )
+import System.Directory                     ( doesFileExist )
 
 readOrCreate :: FilePath -> IO String
 readOrCreate filePath = do
     exists <- doesFileExist filePath
     if exists then readFile filePath else return ""
+
+(+>) :: B.ByteString -> Handler b service () -> (B.ByteString, Handler b service ())
+(+>) = mkRoute
+
+mkRoute :: B.ByteString -> Handler b service () -> (B.ByteString, Handler b service ())
+mkRoute path service = (path, mkCORS service)
+
+mkCORS :: Handler b service () -> Handler b service ()
+mkCORS = applyCORS defaultOptions
