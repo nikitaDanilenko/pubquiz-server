b20d5e997f05400fae4a719c72c71dab9aaac0e4
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-22 17:49:52 +0200

    Add duplicate of the labels used by pubquiz-server

---
+++ src/Labels.hs
@@ -0,0 +1,47 @@
+module Labels ( Labels, defaultLabels, mkLabels ) where
+
+data Labels = Labels { 
+  roundLabel :: String,
+  groupLabel :: String,
+  ownPointsLabel :: String, 
+  maxReachedLabel :: String,
+  maxReachableLabel :: String,
+  backToChartView :: String,
+  mainLabel :: String,
+  ownPageLabel :: String
+} deriving (Show, Read)
+
+mkLabels :: String -> String -> String -> String -> String -> String -> String -> String -> Labels
+mkLabels roundLbl groupLbl ownPointsLbl maxReachedLbl maxReachableLbl backLbl mainLbl ownPageLbl =
+    Labels {
+        roundLabel = htmlSafeString roundLbl,
+        groupLabel = htmlSafeString groupLbl,
+        ownPointsLabel = htmlSafeString ownPointsLbl,
+        maxReachedLabel = htmlSafeString maxReachedLbl,
+        maxReachableLabel = htmlSafeString maxReachableLbl,
+        backToChartView = backLbl,
+        mainLabel = mainLbl,
+        ownPageLabel = ownPageLbl
+    } 
+
+htmlSafeChar :: Char -> String
+htmlSafeChar 'ö' = "&ouml;"
+htmlSafeChar 'ä' = "&auml;"
+htmlSafeChar 'ü' = "&uuml;"
+htmlSafeChar 'ß' = "&szlig;"
+htmlSafeChar c = [c]
+
+htmlSafeString :: String -> String
+htmlSafeString = concatMap htmlSafeChar
+
+defaultLabels :: Labels
+defaultLabels = Labels { 
+  roundLabel = htmlSafeString "Runde",
+  groupLabel = htmlSafeString "Gruppe",
+  ownPointsLabel = htmlSafeString "Erreichte Punkte", 
+  maxReachedLabel = htmlSafeString "Erreichte Höchstpunktzahl",
+  maxReachableLabel = htmlSafeString "Erreichbare Punkte",
+  backToChartView = htmlSafeString "Gesamtansicht",
+  mainLabel = htmlSafeString "Pubquiz",
+  ownPageLabel = htmlSafeString "Eigene Punkte"
+}
