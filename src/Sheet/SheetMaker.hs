5117170e3452d383fcdde08ca436f5fa52ef0d04
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-05-27 21:34:34 +0200

    Fix wrong relative and absolute link paths, add prefix to sheet file

--- src/Sheet/SheetMaker.hs
+++ src/Sheet/SheetMaker.hs
@@ -1,4 +1,4 @@
-module SheetMaker ( createSheetWith ) where
+module Sheet.SheetMaker ( createSheetWith ) where
 
 import System.Directory   ( setCurrentDirectory, getCurrentDirectory )
 import System.Process     ( callProcess )
@@ -7,7 +7,26 @@ import Constants          ( quizzesFolderIO, addSeparator )
 import Sheet.Tex          ( mkSheet )
 
 type Prefix = String
+type Server = String
 type Ending = String
+
+createSheetWith :: String -> Int -> Prefix -> Server -> [Ending] -> IO ()
+createSheetWith groupLabel rounds prefix server endings = do
+    quizzesFolder <- quizzesFolderIO
+    currentDir <- getCurrentDirectory
+    let fullPath = addSeparator [quizzesFolder, prefix]
+        fullServerPath = addSeparator [server, quizzesFolder, prefix, ""]
+        sht = mkSheet groupLabel rounds
+        sheetFile = mkSheetFile prefix
+        texFile = concat [sheetFile, ".tex"]
+    setCurrentDirectory fullPath
+    writeFile texFile sht 
+    mapM_ (createQR fullServerPath) endings
+
+    createPDF texFile endings
+    cleanImages endings
+    cleanLatex sheetFile
+    setCurrentDirectory currentDir
 
 createQR :: Prefix -> Ending -> IO ()
 createQR prefix ending = 
@@ -18,7 +37,7 @@ variableFile = "listVariable.tex"
 
 createLatexVariable :: [Ending] -> IO ()
 createLatexVariable es = writeFile variableFile variable where
-    indexed = zip [1 ..] es
+    indexed = zip [1 :: Int ..] es
 
     variable :: String
     variable = unlines (map (\(i, str) -> concat ["\\mkRounds{", show i, "}{", str, "}" ]) indexed)
@@ -26,35 +45,20 @@ createLatexVariable es = writeFile variableFile variable where
 sheet :: String
 sheet = "Sheet"
 
-sheetTex :: String
-sheetTex = concat [sheet, ".tex"]
+mkSheetFile :: Prefix -> String
+mkSheetFile prefix = concat [prefix, "-", sheet]
 
-createPDF :: [Ending] -> IO ()
-createPDF es = do
+createPDF :: String -> [Ending] -> IO ()
+createPDF texFile es = do
     createLatexVariable es
-    callProcess "pdflatex" [sheetTex]
+    callProcess "pdflatex" [texFile]
 
-cleanLatex :: IO ()
-cleanLatex = callProcess "rm" [concat [sheet, ".log"], 
-                               concat [sheet, ".aux"],
-                               concat [sheet, ".tex"],
-                               variableFile
-                               ]
+cleanLatex :: String -> IO ()
+cleanLatex sheetFile = callProcess "rm" [concat [sheetFile, ".log"], 
+                                         concat [sheetFile, ".aux"],
+                                         concat [sheetFile, ".tex"],
+                                         variableFile
+                                        ]
 
 cleanImages :: [Ending] -> IO ()
 cleanImages es = callProcess "rm" (map (++ ".png") es)
-
-createSheetWith :: String -> Int -> Prefix -> [Ending] -> IO ()
-createSheetWith groupLabel rounds prefix endings = do
-    quizzesFolder <- quizzesFolderIO
-    currentDir <- getCurrentDirectory
-    let fullPath = addSeparator [quizzesFolder, prefix]
-        sheet = mkSheet groupLabel rounds
-    setCurrentDirectory fullPath
-    writeFile sheetTex sheet 
-    mapM_ (createQR prefix) endings
-
-    createPDF endings
-    cleanImages endings
-    cleanLatex
-    setCurrentDirectory currentDir
