33b5b6dca0b2e456e6a5d95d85cbc9c9608ea81b
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-06-06 07:56:32 +0200

    Extract some common HTML utilities into a separate file

---
+++ src/Pages/HtmlUtil.hs
@@ -0,0 +1,39 @@
+module Pages.HtmlUtil where
+
+import Prelude hiding ( div )
+
+centerDiv :: String -> String
+centerDiv = div . tagged "center"
+
+div :: String -> String
+div = tagged "div"
+
+h1 :: String -> String
+h1 = tagged "h1"
+
+h1With :: String -> String -> String
+h1With attrs text = concat [openWith, text, close] where
+  (_, close) = tag "h1"
+  openWith = concat ["<h1 ", attrs, ">"]
+
+tableCell :: String -> String
+tableCell = tagged "td"
+
+tableRow :: String -> String
+tableRow  = (++ "\n") . tagged "tr"
+
+headerCell :: String -> String
+headerCell = tagged "th"
+
+tag :: String -> (String, String)
+tag t = (concat ["<", t, ">"], concat ["</", t, ">"])
+
+tagged :: String -> String -> String
+tagged t text = concat [open, text, close] where
+  (open, close) = tag t
+
+mkButton :: String -> String
+mkButton text = concat ["<a href=\"./index.html\" class=\"button\">", text, "</a>"]
+
+pageHeader :: String
+pageHeader = "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML//EN\">\n"
