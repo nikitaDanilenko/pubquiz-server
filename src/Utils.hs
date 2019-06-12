92d490e4619e6c81ad98f638ba5fe1bc98a660b5
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-06-12 16:52:01 +0200

    Add function for the production of an arbitrary amount of short, distinct strings (via disambiguation)

--- src/Utils.hs
+++ src/Utils.hs
@@ -2,10 +2,14 @@
 
 module Utils where
 
+import Control.Arrow                        ( second )
 import "cryptonite" Crypto.Hash             ( Digest, hash )
 import "cryptonite" Crypto.Hash.Algorithms  ( SHA512 )
 import Data.Char                            ( chr )
 import qualified Data.ByteString.Char8 as B 
+import Data.Function                        ( on )
+import Data.List                            ( sortBy, groupBy )
+import Data.Ord                             ( comparing )
 import Snap.Snaplet                         ( Handler )
 import Snap.Util.CORS                       ( applyCORS, defaultOptions )
 import System.Directory                     ( doesFileExist )
@@ -32,6 +36,40 @@ mkCORS = applyCORS defaultOptions
 
 randomStringIO :: IO String
 randomStringIO = fmap (map chr . randomRs (33, 126)) newStdGen
+
+randomAlphaNumeric :: IO String
+randomAlphaNumeric = fmap (map (chars !!) . randomRs (0, charsLength - 1)) newStdGen where
+    chars = ['0' .. '9'] ++ ['a' .. 'z']
+    charsLength = length chars
+
+randomDistinctAlphaNumeric :: Int -> Int -> IO [String]
+randomDistinctAlphaNumeric numberOfStrings size = do
+    randomInfinite <- randomAlphaNumeric
+    let chunks = take numberOfStrings (chunk size randomInfinite)
+        uniqueChunks = disambiguate chunks
+    return uniqueChunks
+
+chunk :: Int -> [a] -> [[a]]
+chunk size xs | size <= 0 = [xs]
+              | null xs = []
+              | otherwise = h : chunk size t where (h, t) = splitAt size xs
+
+disambiguate :: [String] -> [String]
+disambiguate = 
+    map snd . 
+        sortBy (comparing fst) .
+        concatMap (uncurry zip . second disambiguateList . unzip) . 
+        groupBy ((==) `on` snd) . 
+        sortBy (comparing snd) . 
+        zip [0 ..]
+
+disambiguateSingle :: Int -> String -> String
+disambiguateSingle = (++) . show
+
+disambiguateList :: [String] -> [String]
+disambiguateList [] = []
+disambiguateList [s] = [s]
+disambiguateList xs = zipWith disambiguateSingle [0 ..] xs
 
 type Hashed = Digest SHA512
 
