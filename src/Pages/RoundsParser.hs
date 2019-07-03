b040fa6e3796ebce96bb7d2cc5bc767132f60b61
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-07-03 07:18:27 +0200

    Add a parser for alternatives between only codes and codes with names

---
+++ src/Pages/RoundsParser.hs
@@ -0,0 +1,37 @@
+module Pages.RoundsParser ( parseCodesWithMaybeNames ) where
+
+import Control.Applicative           ( (<|>) )
+import Text.Parsec.Prim              ( parse )
+import Text.ParserCombinators.Parsec ( Parser, many1, oneOf, spaces, char, anyChar, sepBy, noneOf )
+import Utils                         ( alphaNumeric )
+
+codeParser :: Parser String
+codeParser = many1 (oneOf alphaNumeric)
+
+codeWithNameParser :: Parser (String, Maybe String)
+codeWithNameParser = do
+    char '('
+    spaces
+    code <- codeParser
+    spaces
+    char '|'
+    spaces
+    char '\\'
+    name <- fmap Just (many1 (noneOf "\\")) <|> pure Nothing
+    char '\\'
+    spaces
+    char ')'
+    return (code, name)
+
+codeWithMaybeNameParser :: Parser (String, Maybe String)
+codeWithMaybeNameParser =
+    fmap (\s -> (s, Nothing)) codeParser <|> codeWithNameParser
+
+codesWithMaybeNamesParser :: Parser [(String, Maybe String)]
+codesWithMaybeNamesParser = sepBy codeWithMaybeNameParser spaces
+
+parseCodesWithMaybeNames :: String -> [(String, Maybe String)]
+parseCodesWithMaybeNames text = 
+    case parse codesWithMaybeNamesParser "" text of
+        Left _       -> []
+        Right result -> result
