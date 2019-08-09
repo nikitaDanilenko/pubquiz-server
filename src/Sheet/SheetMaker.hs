67ef99cc76c2cdf43a7df9a8e5842aec874edbea
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-08-09 19:59:10 +0200

    Provide a function for creating sheets with the new, Hatex-based function only

--- src/Sheet/SheetMaker.hs
+++ src/Sheet/SheetMaker.hs
@@ -1,14 +1,17 @@
 module Sheet.SheetMaker ( createSheetWith, defaultEndings, Ending ) where
 
-import Control.Exception       ( catch )
-import Control.Exception.Base  ( IOException )
-import Control.Monad           ( when, void )
-import System.Directory        ( setCurrentDirectory, getCurrentDirectory, removeFile )
-import System.Process          ( callProcess )
-     
-import Constants               ( quizzesFolderIO, addSeparator )
-import Pages.HtmlUtil          ( unEscape )
-import Sheet.Tex               ( mkSheet )
+import Control.Exception           ( catch )
+import Control.Exception.Base      ( IOException )
+import Control.Monad               ( when, void )
+import Data.Text                   ( Text )
+import qualified Data.Text as T    ( pack, concat )
+import qualified Data.Text.IO as I ( writeFile )
+import System.Directory            ( setCurrentDirectory, getCurrentDirectory, removeFile )
+import System.Process              ( callProcess )
+
+import Constants                   ( quizzesFolderIO, addSeparator )
+import Pages.HtmlUtil              ( unEscape )
+import Sheet.Tex                   ( mkSheet, mkSheetWithConstantQuestions )
 
 type Prefix = String
 type Server = String
@@ -47,6 +50,30 @@ defaultEndings = [
         "yle8rf", "mdl20a", "84hrui", "c8vb3w", "la9inh",
         "k6bghz", "jwb54g", "4geu7y", "nbc6t4", "amv6zh"
     ]
+
+createQRPath :: Prefix -> Ending -> Text
+createQRPath prefix ending = T.concat (map T.pack [prefix, ending, ".html"])
+
+createSheetWith2 :: String -> Int -> Prefix -> Server -> [Ending] -> IO ()
+createSheetWith2 teamLabel rounds prefix server endings = do
+    quizzesFolder <- quizzesFolderIO
+    currentDir <- getCurrentDirectory
+    
+    let sheet = mkSheetWithConstantQuestions (T.pack teamLabel) rounds paths
+        fullServerPath = addSeparator [server, prefix, ""]
+        paths = map (createQRPath fullServerPath) endings
+        buildPath = addSeparator [quizzesFolder, prefix]
+        sheetFile = mkSheetFile prefix
+        texFile = sheetFile ++ ".tex"
+    
+    setCurrentDirectory buildPath
+    I.writeFile texFile sheet
+
+    createPDF texFile endings `catch` noPDFLatex
+    cleanLatex sheetFile
+    setCurrentDirectory currentDir
+  where noPDFLatex :: IOException -> IO ()
+        noPDFLatex _ = putStrLn "pdflatex not found or it failed during document creation."
 
 createQR :: Prefix -> Ending -> IO ()
 createQR prefix ending = 
