e1a81f98253047b1184f5dbcf0f796e62d95fab9
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-08-12 15:26:26 +0200

    Simplify the creation of answer sheets

--- src/Sheet/SheetMaker.hs
+++ src/Sheet/SheetMaker.hs
@@ -17,32 +17,6 @@ type Prefix = String
 type Server = String
 type Ending = String
 
-createSheetWith :: String -> Int -> Prefix -> Server -> [Ending] -> IO ()
-createSheetWith teamLabel rounds prefix server endings = do
-    quizzesFolder <- quizzesFolderIO
-    currentDir <- getCurrentDirectory
-    let fullPath = addSeparator [quizzesFolder, prefix]
-        fullServerPath = addSeparator [server, prefix, ""]
-        sht = mkSheet (safeTeX teamLabel) rounds
-        sheetFile = mkSheetFile prefix
-        texFile = sheetFile ++ ".tex"
-
-    setCurrentDirectory fullPath
-
-    writeFile texFile sht 
-    
-    b <- (mapM_ (createQR fullServerPath) endings >> return True) `catch` noQREncode
-    when b (createPDF texFile endings `catch` noPDFLatex)
-
-    cleanImages endings
-    cleanLatex sheetFile
-    setCurrentDirectory currentDir
-  where noQREncode :: IOException -> IO Bool
-        noQREncode _ = putStrLn "qrencode not found. No sheet created." >> return False
-
-        noPDFLatex :: IOException -> IO ()
-        noPDFLatex _ = putStrLn "pdflatex not found or it failed during document creation."
-
 defaultEndings :: [Ending]
 defaultEndings = [
         "sdig1o", "aikp25", "vzt35d", "fs7g5r", "9hf347",
@@ -54,8 +28,8 @@ defaultEndings = [
 createQRPath :: Prefix -> Ending -> Text
 createQRPath prefix ending = T.concat (map T.pack [prefix, ending, ".html"])
 
-createSheetWith2 :: String -> Int -> Prefix -> Server -> [Ending] -> IO ()
-createSheetWith2 teamLabel rounds prefix server endings = do
+createSheetWith :: String -> Int -> Prefix -> Server -> [Ending] -> IO ()
+createSheetWith teamLabel rounds prefix server endings = do
     quizzesFolder <- quizzesFolderIO
     currentDir <- getCurrentDirectory
     
@@ -69,25 +43,11 @@ createSheetWith2 teamLabel rounds prefix server endings = do
     setCurrentDirectory buildPath
     I.writeFile texFile sheet
 
-    createPDF texFile endings `catch` noPDFLatex
+    createPDF texFile `catch` noPDFLatex
     cleanLatex sheetFile
     setCurrentDirectory currentDir
   where noPDFLatex :: IOException -> IO ()
         noPDFLatex _ = putStrLn "pdflatex not found or it failed during document creation."
-
-createQR :: Prefix -> Ending -> IO ()
-createQR prefix ending = 
-    callProcess "qrencode" [concat [prefix, ending, ".html"], concat ["-o", ending, ".png"]]
-
-variableFile :: String
-variableFile = "listVariable.tex"
-
-createLatexVariable :: [Ending] -> IO ()
-createLatexVariable es = writeFile variableFile variable where
-    indexed = zip [1 :: Int ..] es
-
-    variable :: String
-    variable = unlines (map (\(i, str) -> concat ["\\mkRounds{", show i, "}{", str, "}" ]) indexed)
 
 sheet :: String
 sheet = "Sheet"
@@ -95,22 +55,16 @@ sheet = "Sheet"
 mkSheetFile :: Prefix -> String
 mkSheetFile prefix = concat [prefix, "-", sheet]
 
-createPDF :: String -> [Ending] -> IO ()
-createPDF texFile es = do
-    createLatexVariable es
-    callProcess "pdflatex" ["-interaction=nonstopmode", texFile]
+createPDF :: String -> IO ()
+createPDF texFile = callProcess "pdflatex" ["-interaction=nonstopmode", texFile]
 
 cleanLatex :: String -> IO ()
-cleanLatex sheetFile = 
-    mapM_  safeRemoveFile (variableFile : map (sheetFile ++) [".log", ".aux", ".tex"])
+cleanLatex sheetFile = mapM_  safeRemoveFile (map (sheetFile ++) [".log", ".aux", ".tex"])
 
 safeRemoveFile :: String -> IO ()
 safeRemoveFile path = removeFile path `catch` noFile where
     noFile :: IOException -> IO ()
     noFile _ = void (putStrLn "No file to remove")
-
-cleanImages :: [Ending] -> IO ()
-cleanImages = mapM_ (safeRemoveFile . (++ ".png"))
 
 safeTeX :: String -> String
 safeTeX = concatMap safeTeXChar . unEscape
