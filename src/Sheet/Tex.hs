7af388a69e2b675b446dd2d8b3ebb8ef9bd1a998
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-08-12 15:35:46 +0200

    Remove old implementation

--- src/Sheet/Tex.hs
+++ src/Sheet/Tex.hs
@@ -1,6 +1,6 @@
 {-# Language OverloadedStrings #-}
 
-module Sheet.Tex ( mkSheet, mkSheetWithArbitraryQuestions, mkSheetWithConstantQuestions ) where
+module Sheet.Tex ( mkSheetWithArbitraryQuestions, mkSheetWithConstantQuestions ) where
 
 import Data.List       ( intercalate, intersperse )
 import Data.List.Split ( chunksOf )
@@ -125,18 +125,6 @@ mkSheetWithConstantQuestions :: Text -> Int -> [Text] -> Text
 mkSheetWithConstantQuestions teamLabel n = 
     mkSheetWithArbitraryQuestions teamLabel (replicate n fittingPerRound)
 
-mkSheet :: String -> Int -> String
-mkSheet teamLabel n = concat [
-    header,
-    grp,
-    centre,
-    perGroup n,
-    footer teamLabel
-    ]
-    where grp = unwords ["    {\\Huge", 
-                         teamLabel, 
-                         "#1:} & \\includegraphics[align=c, scale = 0.35]{./#2.png}"]
-
 mkAnswerTable :: LaTeXC l => Double -> Int -> l
 mkAnswerTable sf qs = 
     table [ForcePos, Here] (
@@ -158,71 +146,3 @@ mkAnswerTable sf qs =
                   )
                  )
     )
-
-header :: String
-header = unlines [
-    "\\documentclass[]{scrartcl}",
-    "\\usepackage{inputenc}",
-    "\\usepackage[ngerman]{babel}",
-    "\\usepackage{amssymb,amsmath}",
-    "\\usepackage{graphbox}",
-    "",
-    "\\usepackage{mathpazo, geometry, forloop, float, graphicx}",
-    "",
-    "\\geometry{left=1cm,textwidth=19cm,top=1cm,textheight=27cm}",
-    "\\newcounter{question}",
-    "",
-    "\\pagestyle{empty}",
-    "",
-    "\\newcommand{\\mkTable}{%",
-    "\\renewcommand{\\arraystretch}{2.75}",
-    "\\begin{table}[H]",
-    " \\centering",
-    " \\begin{tabular*}{\\textwidth}{l}",
-    " \\forloop{question}{1}{\\value{question} < 9}{",
-    "  {\\Large \\arabic{question}.}\\\\\\hline",
-    " }",
-    " \\end{tabular*}",
-    "",
-    "\\end{table}",
-    "}",
-    "",
-    "\\newcommand{\\mkHeader}[2]{",
-    "\\begin{table}[H]",
-    " \\centering",
-    " \\begin{tabular*}{\\textwidth}{l @{\\extracolsep{\\fill}} r}"
-    ]
-
-centre :: String
-centre = unlines [
-    " \\end{tabular*}",
-    "\\end{table}",
-    "}",
-    "",
-    "\\newcommand{\\mkRounds}[2]{%",
-    "\\mkHeader{#1}{#2}"
-    ]
-    
-simpleHeader :: String -> String
-simpleHeader teamLabel = unlines [
-        "\\newcommand{\\simpleHeader}[1]{%",
-        unwords ["    {\\Huge", teamLabel, "#1:}"],
-        "}"
-    ]
-
-perGroup :: Int -> String
-perGroup n = unlines (intercalate ["\\newpage", "\\simpleHeader{#1}"] 
-                                  (chunksOf 2 (replicate n "\\mkTable")) ++ ["\\newpage"])
-
-footer :: String -> String
-footer teamLabel = unlines [
-    "}",
-    "",
-    simpleHeader teamLabel,
-    "",
-    "\\begin{document}",
-    "",
-    "\\input{listVariable}",
-    "",
-    "\\end{document}"
-    ]
