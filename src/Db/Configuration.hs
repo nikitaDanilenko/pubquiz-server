20189484d035f472f693d882011d163a503ce2c6
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-02 18:15:18 +0100

    Add functionality to read global database configuration parameters from file.

--- src/Db/Configuration.hs
+++ src/Db/Configuration.hs
@@ -1,12 +1,17 @@
 module Db.Configuration where
 
-data Configuration = Configuration {
-  host :: String,
-  name :: String,
-  user :: String,
-  password :: String,
-  port :: String
-}
+import           Constants     (configMap, databaseHost, databaseName,
+                                databasePassword, databasePort, databaseUser)
+import           Data.Map.Lazy ((!?))
+
+data Configuration =
+  Configuration
+    { host     :: String
+    , name     :: String
+    , user     :: String
+    , password :: String
+    , port     :: String
+    }
 
 toConnection :: Configuration -> String
 toConnection c =
@@ -15,3 +20,13 @@ toConnection c =
        (\k v -> concat [k, "=", v])
        ["host", "dbname", "user", "password", "port"]
        [host c, name c, user c, password c, port c])
+
+readConfiguration :: IO Configuration
+readConfiguration = do
+  settings <- configMap
+  let parameters = traverse (settings !?) [databaseHost, databaseName, databaseUser, databasePassword, databasePort]
+      config =
+        case parameters of
+          Just (h:n:u:pw:p:_) -> Configuration h n u pw p
+          _ -> error "Database connection not fully specified"
+  pure config
