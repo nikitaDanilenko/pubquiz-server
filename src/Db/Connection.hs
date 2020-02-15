b295ef804db72ce62981f27a367cc3bfc5130c6c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-15 14:51:42 +0100

    #2: Add smart constructors for db types

--- src/Db/Connection.hs
+++ src/Db/Connection.hs
@@ -12,6 +12,7 @@
 
 module Db.Connection where
 
+import           Api.Services.SavedUser                (UserName)
 import           Control.Monad.IO.Class                (MonadIO, liftIO)
 import           Control.Monad.Logger                  (NoLoggingT,
                                                         runStderrLoggingT)
@@ -23,6 +24,27 @@ import           Database.Persist.Postgresql
 import           Database.Persist.TH
 import           Db.Configuration                      (readConfiguration,
                                                         toConnection)
+import           Db.DbTypes                            (Activity,
+                                                        BackToChartViewLabel,
+                                                        Code, CumulativeLabel,
+                                                        IndividualRoundsLabel,
+                                                        MainLabel,
+                                                        MaxReachableLabel,
+                                                        MaxReachedLabel,
+                                                        OwnPageLabel,
+                                                        OwnPointsLabel, Place,
+                                                        PlaceLabel,
+                                                        PlacementLabel,
+                                                        PointsLabel,
+                                                        ProgressionLabel,
+                                                        QuizDate, QuizName,
+                                                        RoundLabel, RoundNumber,
+                                                        RoundWinnerLabel,
+                                                        TeamLabel, TeamName,
+                                                        TeamNumber,
+                                                        Unwrappable (unwrap),
+                                                        UserHash, UserSalt,
+                                                        ViewPreviousLabel)
 import           Db.Instances
 import           GHC.Natural                           (Natural)
 
@@ -84,6 +106,60 @@ DbUser
   UniqueDbUserUserName userName
   deriving Show
 |]
+
+mkDbQuiz :: Place -> QuizDate -> QuizName -> Activity -> DbQuiz
+mkDbQuiz p qd qn a = DbQuiz (unwrap p) (unwrap qd) (unwrap qn) (unwrap a)
+
+mkDbLabels ::
+     DbQuizId
+  -> RoundLabel
+  -> TeamLabel
+  -> OwnPointsLabel
+  -> MaxReachedLabel
+  -> MaxReachableLabel
+  -> BackToChartViewLabel
+  -> MainLabel
+  -> OwnPageLabel
+  -> ViewPreviousLabel
+  -> CumulativeLabel
+  -> IndividualRoundsLabel
+  -> ProgressionLabel
+  -> PlacementLabel
+  -> PlaceLabel
+  -> PointsLabel
+  -> RoundWinnerLabel
+  -> DbLabels
+mkDbLabels qid rd t own mr mred btc m op vp c ir pr plcmt plc pts rw =
+  DbLabels
+    qid
+    (unwrap rd)
+    (unwrap t)
+    (unwrap own)
+    (unwrap mr)
+    (unwrap mred)
+    (unwrap btc)
+    (unwrap m)
+    (unwrap op)
+    (unwrap vp)
+    (unwrap c)
+    (unwrap ir)
+    (unwrap pr)
+    (unwrap plcmt)
+    (unwrap plc)
+    (unwrap pts)
+    (unwrap rw)
+
+mkDbTeamNameCode :: DbQuizId -> TeamNumber -> Code -> TeamName -> Activity -> DbTeamNameCode
+mkDbTeamNameCode qid tn c tl a = DbTeamNameCode qid (unwrap tn) (unwrap c) (unwrap tl) (unwrap a)
+
+mkDbRoundReachable :: DbQuizId -> RoundNumber -> Double -> DbRoundReachable
+mkDbRoundReachable qid rn = DbRoundReachable qid (unwrap rn)
+
+mkDbRoundReached :: DbQuizId -> RoundNumber -> TeamNumber -> Double -> DbRoundReached
+mkDbRoundReached qid rn tn = DbRoundReached qid (unwrap rn) (unwrap tn)
+
+mkDbUser :: UserName -> UserSalt -> UserHash -> DbUser
+mkDbUser n s h = DbUser (unwrap n) (unwrap s) (unwrap h)
 
 runSql :: ReaderT SqlBackend (NoLoggingT (ResourceT IO)) a -> IO a
 runSql action =
