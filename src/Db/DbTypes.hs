e5bd496065c6ed3e1b3cec624ef1b5908ec37ed1
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-15 15:00:38 +0100

    #2: Add wrap function and corresponding instances for all newtypes

--- src/Db/DbTypes.hs
+++ src/Db/DbTypes.hs
@@ -91,85 +91,114 @@ newtype UserHash =
 
 class Unwrappable t v where
   unwrap :: t -> v
+  wrap :: v -> t
 
 instance Unwrappable TeamNumber Natural where
   unwrap (TeamNumber tn) = tn
+  wrap = TeamNumber
 
 instance Unwrappable RoundNumber Natural where
   unwrap (RoundNumber rn) = rn
+  wrap = RoundNumber
 
 instance Unwrappable Code String where
   unwrap (Code c) = c
+  wrap = Code
 
 instance Unwrappable TeamName String where
   unwrap (TeamName tn) = tn
+  wrap = TeamName
 
 instance Unwrappable QuizName String where
   unwrap (QuizName qn) = qn
+  wrap = QuizName
 
 instance Unwrappable Place String where
   unwrap (Place p) = p
+  wrap = Place
 
 instance Unwrappable Activity Bool where
   unwrap Active   = True
   unwrap Inactive = False
+  wrap False = Inactive
+  wrap True  = Active
 
 instance Unwrappable QuizDate Day where
   unwrap (QuizDate d) = d
+  wrap = QuizDate
 
 instance Unwrappable RoundLabel String where
   unwrap (RoundLabel l) = l
+  wrap = RoundLabel
 
 instance Unwrappable TeamLabel String where
   unwrap (TeamLabel l) = l
+  wrap = TeamLabel
 
 instance Unwrappable OwnPointsLabel String where
   unwrap (OwnPointsLabel l) = l
+  wrap = OwnPointsLabel
 
 instance Unwrappable MaxReachedLabel String where
   unwrap (MaxReachedLabel l) = l
+  wrap = MaxReachedLabel
 
 instance Unwrappable MaxReachableLabel String where
   unwrap (MaxReachableLabel l) = l
+  wrap = MaxReachableLabel
 
 instance Unwrappable BackToChartViewLabel String where
   unwrap (BackToChartViewLabel l) = l
+  wrap = BackToChartViewLabel
 
 instance Unwrappable MainLabel String where
   unwrap (MainLabel l) = l
+  wrap = MainLabel
 
 instance Unwrappable OwnPageLabel String where
   unwrap (OwnPageLabel l) = l
+  wrap = OwnPageLabel
 
 instance Unwrappable ViewPreviousLabel String where
   unwrap (ViewPreviousLabel l) = l
+  wrap = ViewPreviousLabel
 
 instance Unwrappable CumulativeLabel String where
   unwrap (CumulativeLabel l) = l
+  wrap = CumulativeLabel
 
 instance Unwrappable IndividualRoundsLabel String where
   unwrap (IndividualRoundsLabel l) = l
+  wrap = IndividualRoundsLabel
 
 instance Unwrappable ProgressionLabel String where
   unwrap (ProgressionLabel l) = l
+  wrap = ProgressionLabel
 
 instance Unwrappable PlacementLabel String where
   unwrap (PlacementLabel l) = l
+  wrap = PlacementLabel
 
 instance Unwrappable PlaceLabel String where
   unwrap (PlaceLabel l) = l
+  wrap = PlaceLabel
 
 instance Unwrappable PointsLabel String where
   unwrap (PointsLabel l) = l
+  wrap = PointsLabel
 
 instance Unwrappable RoundWinnerLabel String where
   unwrap (RoundWinnerLabel l) = l
+  wrap = RoundWinnerLabel
 
 instance Unwrappable UserName String where
   unwrap (UserName s) = s
+  wrap = UserName
 
 instance Unwrappable UserSalt String where
   unwrap (UserSalt s) = s
+  wrap = UserSalt
 
 instance Unwrappable UserHash String where
   unwrap (UserHash s) = s
+  wrap = UserHash
