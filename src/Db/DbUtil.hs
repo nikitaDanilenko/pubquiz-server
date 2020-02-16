4e14bdc9f788ee77d49563cbe052d022dcb8d648
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-16 23:14:32 +0100

    #2: Add specialised data types for serving points.

---
+++ src/Db/DbUtil.hs
@@ -0,0 +1,38 @@
+module Db.DbUtil where
+
+import           Control.Arrow ((&&&))
+import           Data.Function (on)
+import           Data.List     (groupBy, sortOn)
+import           Data.Map      (fromList, intersectionWith, toList)
+import           Db.Connection (DbRoundReachable (dbRoundReachablePoints, dbRoundReachableRoundNumber),
+                                DbRoundReached (dbRoundReachedPoints, dbRoundReachedRoundNumber, dbRoundReachedTeamNumber))
+import           Db.DbTypes    (RoundNumber (RoundNumber),
+                                TeamNumber (TeamNumber))
+
+data RoundRating =
+  RoundRating
+    { reachableInRound :: Double
+    , points           :: [(TeamNumber, Double)]
+    }
+
+newtype Ratings =
+  Ratings
+    { roundRatings :: [(RoundNumber, RoundRating)]
+    }
+
+-- | Merges a list of reachable points and a list of reached points in their respective database representations
+--   into a unified rating element.
+--   Rounds that are mentioned in only one of the two lists are ignored.
+--   This function ignores the quizzes parameters of both parameters.
+ratingsFromDb :: [DbRoundReachable] -> [DbRoundReached] -> Ratings
+ratingsFromDb reachables reacheds = Ratings (toList (intersectionWith RoundRating reachableMap reachedMap))
+  where
+    reachableMap = fromList (map ((RoundNumber . dbRoundReachableRoundNumber) &&& dbRoundReachablePoints) reachables)
+    reachedMap =
+      fromList
+        (map
+           ((RoundNumber . dbRoundReachedRoundNumber . head) &&&
+            map ((TeamNumber . dbRoundReachedTeamNumber) &&& dbRoundReachedPoints))
+           (groupBy ((==) `on` dbRoundReachedRoundNumber) (sortOn dbRoundReachedRoundNumber reacheds)))
+    mkRoundNumber = undefined
+    mkRoundRating = undefined
