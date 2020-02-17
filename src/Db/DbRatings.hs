b65ab50e97dabc00fb269e685f446242d2b2268a
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-17 21:37:11 +0100

    #2: Make ratings more finely grained (data type) and add JSON conversion

--- src/Db/DbRatings.hs
+++ src/Db/DbRatings.hs
@@ -1,24 +1,39 @@
+{-# LANGUAGE DeriveGeneric     #-}
+
 module Db.DbRatings where
 
 import           Control.Arrow ((&&&))
+import           Data.Aeson    (ToJSON (toJSON), object, (.=))
 import           Data.Function (on)
 import           Data.List     (groupBy, sortOn)
 import           Data.Map      (fromList, intersectionWith, toList)
+import           Data.Text     (pack)
 import           Db.Connection (DbRoundReachable (dbRoundReachablePoints, dbRoundReachableRoundNumber),
                                 DbRoundReached (dbRoundReachedPoints, dbRoundReachedRoundNumber, dbRoundReachedTeamNumber))
 import           Db.DbTypes    (RoundNumber (RoundNumber),
                                 TeamNumber (TeamNumber))
+import           GHC.Generics
+
+data TeamRating = TeamRating {
+  teamNumber :: TeamNumber,
+  rating :: Double
+} deriving Generic
 
 data RoundRating =
   RoundRating
     { reachableInRound :: Double
-    , points           :: [(TeamNumber, Double)]
-    }
+    , points           :: [TeamRating]
+    } deriving Generic
+
+instance ToJSON TeamRating
+instance ToJSON RoundRating
 
 newtype Ratings =
   Ratings
     { roundRatings :: [(RoundNumber, RoundRating)]
-    }
+    } deriving Generic
+
+instance ToJSON Ratings
 
 -- | Merges a list of reachable points and a list of reached points in their respective database representations
 --   into a unified rating element.
@@ -32,7 +47,7 @@ ratingsFromDb reachables reacheds = Ratings (toList (intersectionWith RoundRatin
       fromList
         (map
            ((RoundNumber . dbRoundReachedRoundNumber . head) &&&
-            map ((TeamNumber . dbRoundReachedTeamNumber) &&& dbRoundReachedPoints))
+            map (uncurry TeamRating . ((TeamNumber . dbRoundReachedTeamNumber) &&& dbRoundReachedPoints)))
            (groupBy ((==) `on` dbRoundReachedRoundNumber) (sortOn dbRoundReachedRoundNumber reacheds)))
     mkRoundNumber = undefined
     mkRoundRating = undefined
