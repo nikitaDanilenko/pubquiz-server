c0c5fae0b06e93acfbe5c3c8c0ff4e684675bfbf
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-19 00:21:29 +0100

    #2: Add JSON response utility function

--- src/Api/Services/SnapUtil.hs
+++ src/Api/Services/SnapUtil.hs
@@ -1,7 +1,15 @@
 module Api.Services.SnapUtil where
 
-import qualified Data.ByteString.Char8 as B 
-import Snap.Core                            ( Response, setContentType, setResponseCode )
+import qualified Data.ByteString.Char8 as B
+import           Data.CaseInsensitive  (CI, mk)
+import           Snap.Core             (Response, setContentType, setHeader,
+                                        setResponseCode)
 
 setResponseCodePlain :: Int -> Response -> Response
 setResponseCodePlain code = setResponseCode code . setContentType (B.pack "text/plain")
+
+setResponseCodeJSON :: Int -> Response -> Response
+setResponseCodeJSON code = setResponseCode code . setHeader (mkFromString "Content-Type") (B.pack "application/json")
+
+mkFromString :: String -> CI B.ByteString
+mkFromString = mk . B.pack
