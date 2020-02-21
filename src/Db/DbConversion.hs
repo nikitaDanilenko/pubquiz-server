39a3ec8ed4ecdc41bb86c0e42d1d7abf3bce16a1
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-21 18:01:43 +0100

    #2: Split QuizInfo into two separate types

--- src/Db/DbConversion.hs
+++ src/Db/DbConversion.hs
@@ -8,15 +8,18 @@ import           Data.Function    (on)
 import           Data.List        (groupBy, sortOn)
 import           Data.Map         (fromList, intersectionWith, toList)
 import           Data.Text        (pack)
+import qualified Data.Text        as T
 import           Database.Persist (Entity, entityKey, entityVal)
 import           Db.Connection    (DbQuiz (dbQuizDate, dbQuizName, dbQuizPlace),
                                    DbQuizId,
                                    DbRoundReachable (dbRoundReachablePoints, dbRoundReachableRoundNumber),
                                    DbRoundReached (dbRoundReachedPoints, dbRoundReachedRoundNumber, dbRoundReachedTeamNumber))
+import           General.Labels   (Labels, fallbackLabels)
 import           General.Types    (Place, QuizDate, QuizName,
                                    RoundNumber (RoundNumber),
                                    TeamNumber (TeamNumber), wrap)
 import           GHC.Generics     (Generic)
+import           GHC.Natural      (Natural)
 
 data TeamRating =
   TeamRating
@@ -50,6 +53,30 @@ instance ToJSON Ratings
 
 instance FromJSON Ratings
 
+data Credentials =
+  Credentials
+    { user      :: T.Text
+    , signature :: T.Text
+    }
+  deriving (Generic)
+
+instance FromJSON Credentials
+
+data QuizSettings =
+  QuizSettings
+    { numberOfRounds :: Natural
+    , numberOfTeams  :: Natural
+    , labels         :: Labels
+    }
+  deriving (Generic)
+
+instance ToJSON QuizSettings
+
+instance FromJSON QuizSettings
+
+fallbackSettings :: QuizSettings
+fallbackSettings = QuizSettings {numberOfRounds = 4, numberOfTeams = 20, labels = fallbackLabels}
+
 -- | Merges a list of reachable points and a list of reached points in their respective database representations
 --   into a unified rating element.
 --   Rounds that are mentioned in only one of the two lists are ignored.
@@ -65,12 +92,22 @@ ratingsFromDb reachables reacheds = Ratings (toList (intersectionWith RoundRatin
             map (uncurry TeamRating . ((TeamNumber . dbRoundReachedTeamNumber) &&& dbRoundReachedPoints)))
            (groupBy ((==) `on` dbRoundReachedRoundNumber) (sortOn dbRoundReachedRoundNumber reacheds)))
 
+data QuizPDN =
+  QuizPDN
+    { place :: Place
+    , date  :: QuizDate
+    , name  :: QuizName
+    }
+  deriving (Generic)
+
+instance ToJSON QuizPDN
+
+instance FromJSON QuizPDN
+
 data QuizInfo =
   QuizInfo
-    { quizId :: DbQuizId
-    , name   :: QuizName
-    , date   :: QuizDate
-    , place  :: Place
+    { quizId     :: DbQuizId
+    , identifier :: QuizPDN
     }
   deriving (Generic)
 
@@ -80,6 +117,10 @@ instance FromJSON QuizInfo
 
 mkQuizInfo :: Entity DbQuiz -> QuizInfo
 mkQuizInfo eq =
-  QuizInfo {quizId = entityKey eq, name = wrap (dbQuizName q), date = wrap (dbQuizDate q), place = wrap (dbQuizPlace q)}
+  QuizInfo
+    { quizId = entityKey eq
+    , identifier =
+        QuizPDN {name = wrap (T.pack (dbQuizName q)), date = wrap (dbQuizDate q), place = wrap (T.pack (dbQuizPlace q))}
+    }
   where
     q = entityVal eq
