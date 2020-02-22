ee4137f47fdfee1f1dd96759d4f93439247f2e33
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-22 12:08:08 +0100

    #2: Add temporary duplicate of hash check via JSON and database

--- src/Api/Services/HashCheck.hs
+++ src/Api/Services/HashCheck.hs
@@ -5,17 +5,24 @@ module Api.Services.HashCheck where
 import           Control.Arrow          (second)
 import           Control.Monad.IO.Class (liftIO)
 import qualified Data.ByteString.Char8  as B
+import qualified Data.ByteString.Lazy   as L
 import           Data.Maybe             (fromMaybe, maybe)
+import           Data.Text.Encoding     (decodeUtf8)
 import           Snap.Core              (modifyResponse, setResponseCode,
                                          writeBS)
 import           Snap.Snaplet           (Handler)
 
 import           Constants              (sessionKeysFileIO, userParam)
+import           Data.Aeson             (encode, object, (.=))
 import qualified Data.Text.Encoding     as E
+import           Db.Connection          (dbUserUserHash)
 import           Db.DbConversion        (Credentials, signature)
 import qualified Db.DbConversion        as D
+import           Db.Storage             (findUser)
+import           General.Types          (UserName (UserName))
 import           Utils                  (mkHashed, readOrCreateEmptyBS)
 
+-- todo fuse with the other attempt
 data Attempt =
   Attempt
     { user      :: B.ByteString
@@ -40,6 +47,11 @@ verifyHash attempt sessionKey = mkHash (B.concat [sessionKey, bsArgs]) == claim 
     allArgs = (userParam, user attempt) : arguments attempt
     bsArgs = B.intercalate "&" (map (\(k, v) -> B.concat [k, "=", v]) allArgs)
 
+verifyHash2 :: Attempt2 -> B.ByteString -> Bool
+verifyHash2 attempt sessionKey = mkHash (B.concat [sessionKey, L.toStrict args]) == claim2 attempt
+  where
+    args = encode (object (map (\(k, v) -> decodeUtf8 k .= decodeUtf8 v) (arguments2 attempt)))
+
 toKV :: [B.ByteString] -> (B.ByteString, B.ByteString)
 toKV (key:value:_) = (key, value)
 toKV _             = ("", "")
@@ -55,6 +67,26 @@ verifyHashFromFile attempt = do
 
 mkVerifiedRequest :: Maybe B.ByteString -> Maybe B.ByteString -> [(B.ByteString, Maybe B.ByteString)] -> IO Bool
 mkVerifiedRequest mUser mSig kMvs = verifyHashFromFile (mkAttemptWithMaybe mUser mSig kMvs)
+
+data Attempt2 =
+  Attempt2
+    { user2      :: UserName
+    , arguments2 :: [(B.ByteString, B.ByteString)]
+    , claim2     :: B.ByteString
+    }
+
+mkAttemptWithMaybe2 :: Maybe UserName -> Maybe B.ByteString -> [(B.ByteString, Maybe B.ByteString)] -> Attempt2
+mkAttemptWithMaybe2 mUser mSig kMvs =
+  Attempt2 (fromMaybe (UserName "") mUser) (map (second orEmpty) kMvs) (orEmpty mSig)
+  where
+    orEmpty = fromMaybe ""
+
+mkVerifiedRequest2 :: Maybe UserName -> Maybe B.ByteString -> [(B.ByteString, Maybe B.ByteString)] -> IO Bool
+mkVerifiedRequest2 mUserName mSig kMvs = do
+  mUser <- findUser (user2 attempt)
+  return (maybe False (verifyHash2 attempt . B.pack . dbUserUserHash) mUser)
+  where
+    attempt = mkAttemptWithMaybe2 mUserName mSig kMvs
 
 authenticate ::
      Maybe B.ByteString -> Maybe B.ByteString -> [(B.ByteString, Maybe B.ByteString)] -> Handler b service Bool
