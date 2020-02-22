7c4998aca0ca89d237d159e5b6f72c7a21b2bb7c
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-22 13:53:21 +0100

    #2: Move auxiliary function to utility module

--- src/Api/Services/SnapUtil.hs
+++ src/Api/Services/SnapUtil.hs
@@ -1,6 +1,9 @@
 module Api.Services.SnapUtil where
 
+import           Data.Aeson            (decode)
+import           Data.Aeson.Types      (FromJSON)
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy  as L
 import           Data.CaseInsensitive  (CI, mk)
 import           Snap.Core             (Response, setContentType, setHeader,
                                         setResponseCode)
@@ -13,3 +16,6 @@ setResponseCodeJSON code = setResponseCode code . setHeader (mkFromString "Conte
 
 mkFromString :: String -> CI B.ByteString
 mkFromString = mk . B.pack
+
+attemptDecode :: (Functor f, FromJSON a) => f (Maybe B.ByteString) -> f (Maybe a)
+attemptDecode = fmap (>>= decode . L.fromStrict)
