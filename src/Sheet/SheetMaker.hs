4e3193a11993d3eb98dcd627ad10bcf4efbd5a3f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-02-26 18:44:08 +0100

    #2: Adjust parameters and add an additional sort

--- src/Sheet/SheetMaker.hs
+++ src/Sheet/SheetMaker.hs
@@ -11,6 +11,9 @@ import System.Process              ( callProcess )
 
 import Constants                   ( quizzesFolderIO, addSeparator, sheetsFolderIO )
 import Sheet.Tex                   ( mkSheetWithArbitraryQuestions, mkQROnly )
+import General.Types (TeamNumber, Code, unwrap)
+import Data.List (sortOn)
+import GHC.Natural (Natural)
 
 type Prefix = String
 type Server = String
@@ -19,26 +22,22 @@ type Ending = String
 createQRPath :: Prefix -> Ending -> Text
 createQRPath prefix ending = T.concat (map T.pack [prefix, ending, ".html"])
 
-createSheetWith :: String -> [Int] -> Prefix -> Server -> [Ending] -> IO ()
-createSheetWith teamLabel rounds prefix server endings = do
-    sheetsFolder <- sheetsFolderIO
-    currentDir <- getCurrentDirectory
-    
-    let tl = T.pack teamLabel
-        sht = mkSheetWithArbitraryQuestions tl rounds paths
-        fullServerPath = addSeparator [server, prefix, ""]
-        paths = map (createQRPath fullServerPath) endings
-        sheetFile = mkSheetFile prefix
-
-        qrs = mkQROnly tl paths
-        codesFile = mkCodesFile prefix
-    
-    setCurrentDirectory sheetsFolder
-
-    writeAndCleanPDF sheetFile sht
-    writeAndCleanPDF codesFile qrs
-
-    setCurrentDirectory currentDir
+createSheetWith :: String -> [Int] -> Prefix -> Server -> [(TeamNumber, Code)] -> IO ()
+createSheetWith teamLabel rounds prefix server numberedCodes = do
+  sheetsFolder <- sheetsFolderIO
+  currentDir <- getCurrentDirectory
+  let tl = T.pack teamLabel
+      sht = mkSheetWithArbitraryQuestions tl rounds paths
+      fullServerPath = addSeparator [server, prefix, ""]
+      endings = map (unwrap . snd) (sortOn fst numberedCodes)
+      paths = map (createQRPath fullServerPath) endings
+      sheetFile = mkSheetFile prefix
+      qrs = mkQROnly tl paths
+      codesFile = mkCodesFile prefix
+  setCurrentDirectory sheetsFolder
+  writeAndCleanPDF sheetFile sht
+  writeAndCleanPDF codesFile qrs
+  setCurrentDirectory currentDir
 
 writeAndCleanPDF :: FilePath -> Text -> IO ()
 writeAndCleanPDF mainPath content = do
