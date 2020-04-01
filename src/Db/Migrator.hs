ba87744b8d6e9ea11fe91e5361995191c214d5db
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-04-02 00:34:14 +0200

    #12: Add all relevant paths to old quiz ratings

--- src/Db/Migrator.hs
+++ src/Db/Migrator.hs
@@ -79,7 +79,51 @@ parseQuiz text = QuizRatings (Header header) ratings
 
 writeQuiz :: String -> String -> String -> IO ()
 writeQuiz name date ratings = do
-  quizInfo <- createQuiz (QuizIdentifier (wrap name) (wrap (read date :: Day)) (wrap ("Café Godot" :: T.Text)))
+  quizInfo <- createQuiz (QuizIdentifier (wrap ("Café Godot" :: T.Text)) (wrap (read date :: Day)) (wrap name))
   let qid = quizId quizInfo
   setLabels qid fallback
   setQuizRatings qid (parseQuiz ratings)
+
+writeQuizFromFile :: String -> IO ()
+writeQuizFromFile filePath = do
+  text <- readFile filePath
+  let name : date : ratingsLines = lines text
+  writeQuiz name date (unlines ratingsLines)
+
+writeAll :: IO ()
+writeAll =
+  mapM_ (\folder -> writeQuizFromFile ("remote/" ++ folder ++ "/rounds.txt"))
+    [
+      "2019-02-16",
+      "2019-05-15",
+      "2019-05-22",
+      "2019-05-29",
+      "2019-06-03",
+      "2019-06-12",
+      "2019-06-19",
+      "2019-07-03",
+      "2019-07-17",
+      "2019-07-26",
+      "2019-07-31",
+      "2019-08-14",
+      "2019-09-04",
+      "2019-09-18",
+      "2019-10-09",
+      "2019-10-16",
+      "2019-10-23",
+      "2019-10-30",
+      "2019-11-06",
+      "2019-11-13",
+      "2019-11-20",
+      "2019-11-27",
+      "2019-12-04",
+      "2019-12-11",
+      "2019-12-18",
+      "2020-01-08",
+      "2020-01-29",
+      "2020-02-19",
+      "2020-02-26",
+      "2020-03-18",
+      "2020-03-25",
+      "2020-04-01"
+    ]
