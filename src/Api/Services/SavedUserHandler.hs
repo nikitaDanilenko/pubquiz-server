d46d6f45af136a168eee179c5cdaf2bd5f4da72d
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-12-03 20:35:16 +0100

    Refactor: Switch to Either for transformers

--- src/Api/Services/SavedUserHandler.hs
+++ src/Api/Services/SavedUserHandler.hs
@@ -3,20 +3,21 @@
 module Api.Services.SavedUserHandler
   ( mkHash
   , mkAndSaveUser
-  , Status(..)
   , Password
   ) where
 
-import qualified Data.Text              as T
-import qualified Data.Text.Encoding     as E
+import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString.Lazy  as L
+import qualified Data.Text             as T
+import qualified Data.Text.Encoding    as E
 
-import           Constants              (saltSize)
-import           Db.DbConversion        (SavedUser (SavedUser))
-import           Db.Storage             (findUser, setUser)
-import           General.Types          (Password, UserCreation (UserCreation),
-                                         UserHash, UserName, UserSalt, unwrap,
-                                         wrap)
-import           Utils                  (Hashed, mkHashed, randomStringIO)
+import           Constants             (saltSize)
+import           Db.DbConversion       (SavedUser (SavedUser))
+import           Db.Storage            (findUser, setUser)
+import           General.Types         (Password, UserCreation (UserCreation),
+                                        UserHash, UserName, UserSalt, unwrap,
+                                        wrap)
+import           Utils                 (Hashed, mkHashed, randomStringIO)
 
 mkUser :: UserName -> Password -> IO SavedUser
 mkUser user pass = do
@@ -26,21 +27,15 @@ mkUser user pass = do
       savedUser = SavedUser user salt hashValue
   return savedUser
 
-mkAndSaveUser :: UserCreation -> IO Status
+mkAndSaveUser :: UserCreation -> IO (Either L.ByteString ())
 mkAndSaveUser (UserCreation user pass) = do
   mDbUser <- findUser user
   case mDbUser of
-    Just _ -> do
-      putStrLn (unwords ["User", unwrap user, "already exists.", "Nothing changed."])
-      pure (Exists user)
+    Just _ -> pure (Left (L.fromStrict (B.unwords ["User", unwrap user, "already exists.", "Nothing changed."])))
     Nothing -> do
       newUser <- mkUser user pass
       setUser newUser
-      pure Success
-
-data Status
-  = Success
-  | Exists UserName
+      pure (Right ())
 
 mkUserHashed :: UserName -> Password -> UserSalt -> Hashed
 mkUserHashed user pass salt = mkHashed (E.encodeUtf8 (T.concat [unwrap user, unwrap pass, unwrap salt]))
