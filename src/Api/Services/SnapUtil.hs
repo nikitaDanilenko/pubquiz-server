93a6f29ce09b111826c9f4310e0de7af25b40031
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2020-12-03 20:37:23 +0100

    Refactor: Adjust utility functions around ExceptT

--- src/Api/Services/SnapUtil.hs
+++ src/Api/Services/SnapUtil.hs
@@ -1,22 +1,26 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MonoLocalBinds   #-}
+{-# LANGUAGE TupleSections    #-}
 
 module Api.Services.SnapUtil where
 
-import           Control.Applicative       (liftA2)
-import           Control.Monad.Trans.Maybe (MaybeT (MaybeT))
-import           Data.Aeson                (decode)
-import           Data.Aeson.Types          (FromJSON)
-import qualified Data.ByteString.Char8     as B
-import           Data.CaseInsensitive      (CI, mk)
-import qualified Data.Text.Encoding        as E (encodeUtf8)
-import           Data.Word                 (Word64)
-import           Db.DbConversion           (Credentials (Credentials))
-import           General.Types             (Wrapped, wrap)
-import           Snap                      (Handler, readRequestBody)
-import           Snap.Core                 (Request, Response, getHeader,
-                                            getRequest, setHeader,
-                                            setResponseCode)
+import           Control.Applicative        (liftA2)
+import           Control.Monad.Trans.Except (ExceptT (ExceptT))
+import           Data.Aeson                 (eitherDecode)
+import           Data.Aeson.Types           (FromJSON)
+import qualified Data.ByteString.Char8      as B
+import qualified Data.ByteString.Lazy       as L
+import           Data.CaseInsensitive       (CI, mk, original)
+import           Data.Either.Combinators    (mapLeft)
+import           Data.Word                  (Word64)
+import           Db.DbConversion            (Credentials (Credentials))
+import           General.EitherT.Extra      (exceptFromMaybeF)
+import           General.Types              (Wrapped, wrap)
+import           Snap                       (Handler, readRequestBody)
+import           Snap.Core                  (MonadSnap, Request, Response,
+                                             getHeader, getRequest,
+                                             modifyResponse, setHeader,
+                                             setResponseCode, writeLBS)
 
 -- todo: Use general constants for header values
 setResponseCodeJSON :: Int -> Response -> Response
@@ -25,10 +29,14 @@ setResponseCodeJSON code = setResponseCode code . setHeader (mkFromString "Conte
 mkFromString :: String -> CI B.ByteString
 mkFromString = mk . B.pack
 
-readBody :: FromJSON a => Handler b service (Maybe a)
-readBody = fmap decode (readRequestBody maxRequestSize)
+readBody :: FromJSON a => ExceptT L.ByteString (Handler b service) (B.ByteString, a)
+readBody =
+  ExceptT
+    (fmap
+       (\a -> fmap (L.toStrict a, ) (mapLeft (L.fromStrict . B.pack) (eitherDecode a)))
+       (readRequestBody maxRequestSize))
 
-readCredentials :: MaybeT (Handler b service) Credentials
+readCredentials :: ExceptT L.ByteString (Handler b service) Credentials
 readCredentials = liftA2 Credentials (readHeader userHeader) (readHeader signatureHeader)
 
 userHeader :: CI B.ByteString
@@ -40,5 +48,13 @@ signatureHeader = mkFromString "Signature"
 maxRequestSize :: Word64
 maxRequestSize = 65536
 
-readHeader :: Wrapped v B.ByteString => CI B.ByteString -> MaybeT (Handler b service) v
-readHeader hd = fmap wrap (MaybeT (fmap (getHeader hd) getRequest))
+readHeader :: Wrapped v B.ByteString => CI B.ByteString -> ExceptT L.ByteString (Handler b service) v
+readHeader hd = fmap wrap (exceptFromMaybeF (fmap (getHeader hd) getRequest) errorMsg)
+  where
+    errorMsg = L.fromStrict (B.unwords [B.pack "Missing header:", original hd])
+
+jsonResponseCode :: MonadSnap m => Int -> m ()
+jsonResponseCode = modifyResponse . setResponseCodeJSON
+
+errorWithCode :: MonadSnap m => Int -> L.ByteString -> m ()
+errorWithCode c e = writeLBS e >> modifyResponse (setResponseCodeJSON c)
