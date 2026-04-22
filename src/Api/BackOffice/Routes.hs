a9c8d6cdbdbb90ff67f2a6ca9d0bb24cef5a126a
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2026-04-22 21:14:02 +0200

    Generate draft implementation

--- src/Api/BackOffice/Routes.hs
+++ src/Api/BackOffice/Routes.hs
@@ -8,32 +8,35 @@
 
 module Api.BackOffice.Routes where
 
-import           Api.BackOffice.Types        (QuizSummary)
+import           Api.BackOffice.Types        (QuizSummary (..))
 import           Api.Util                    (runDb)
-import           Control.Monad               (forM_)
-import           Core.Domain                 (Place (..), Points, Quiz (..),
-                                              QuizId (..), QuizIdentifier (..),
+import           Control.Monad               (forM, forM_, unless)
+import           Core.Domain                 (NumberOfQuestions (..),
+                                              Place (..), Points (..),
+                                              Quiz (..), QuizId (..),
+                                              QuizIdentifier (..),
                                               QuizName (..), QuizSettings (..),
-                                              QuizState (..), RoundNumber,
-                                              ScoreBoard (..), SomeQuiz,
-                                              Team (..), TeamName (..),
-                                              TeamNumber (..))
+                                              QuizState (..), Round (..),
+                                              RoundNumber (..), ScoreBoard (..),
+                                              SomeQuiz (..), Team (..),
+                                              TeamName (..), TeamNumber (..))
 import           Data.Aeson                  (FromJSON, ToJSON)
+import qualified Data.Map.Strict             as Map
 import           Data.Pool                   (Pool)
 import           Data.Text                   (Text)
-import           Database.Persist.Postgresql (insert)
-import           Database.Persist.Sql        (SqlBackend, fromSqlKey)
-import qualified Db.Schema                   as Db (Quiz (..), QuizId (..),
-                                                    Round (..), Team (..))
+import           Database.Persist            (Entity (..), (=.), (==.))
+import           Database.Persist.Postgresql (delete, get, insert, selectList,
+                                              update, upsert)
+import           Database.Persist.Sql        (SqlBackend, fromSqlKey, toSqlKey)
+import qualified Db.Schema                   as Db
 import           GHC.Generics                (Generic)
 import           Servant
 import           Servant.Auth.Server
 
 -- JWT payload: authenticated organizer
-data AuthenticatedUser = AuthenticatedUser
-  { organizerName :: Text
-  , isAdmin       :: Bool
-  }
+newtype AuthenticatedUser = AuthenticatedUser
+  {    isAdmin       :: Bool
+   }
   deriving (Show, Eq, Generic, FromJSON, ToJSON, ToJWT, FromJWT)
 
 data CreateQuizRequest = CreateQuizRequest
@@ -81,25 +84,73 @@ backOfficeApi = Proxy
 
 backOfficeServer :: Pool SqlBackend -> AuthResult AuthenticatedUser -> Server BackOfficeRoutes
 backOfficeServer pool (Authenticated user) =
-  listQuizzes user
-    :<|> createQuiz pool user
-    :<|> getQuiz user
-    :<|> updateSettings user
-    :<|> updateScore user
-    :<|> lockQuiz user
-    :<|> unlockQuiz user
+  listQuizzes pool
+    :<|> createQuiz pool
+    :<|> getQuiz pool
+    :<|> updateSettings pool
+    :<|> updateScore pool
+    :<|> lockQuiz pool
+    :<|> unlockQuiz pool user
 backOfficeServer _ _ = throwAll err401
 
-listQuizzes :: AuthenticatedUser -> Handler [QuizSummary]
-listQuizzes = undefined
+quizKeyToId :: Db.Key Db.Quiz -> QuizId
+quizKeyToId = QuizId . fromIntegral . fromSqlKey
 
-createQuiz :: Pool SqlBackend -> AuthenticatedUser -> CreateQuizRequest -> Handler (Quiz 'Active)
-createQuiz pool _user request = do
-  quizId <- runDb pool statement
+quizIdToKey :: QuizId -> Db.Key Db.Quiz
+quizIdToKey = toSqlKey . fromIntegral . unQuizId
+
+quizToIdentifier :: Db.Quiz -> QuizIdentifier
+quizToIdentifier quiz =
+  QuizIdentifier
+    { name = QuizName (Db.quizName quiz)
+    , place = Place (Db.quizPlace quiz)
+    , date = Db.quizDate quiz
+    }
+
+dbRoundToRound :: Db.Round -> Round
+dbRoundToRound round =
+  Round
+    { roundNumber = RoundNumber (Db.roundRoundNumber round)
+    , displayMaxPoints = Points (Db.roundReachablePoints round)
+    , numberOfQuestions = Db.roundNumberOfQuestions round
+    }
+
+dbTeamToTeam :: Db.Team -> Team
+dbTeamToTeam team =
+  Team
+    { number = TeamNumber (Db.teamNumber team)
+    , teamName = TeamName (Db.teamName team)
+    , active = Db.teamActive team
+    }
+
+dbScoresToScoreBoard :: [Entity Db.TeamRoundScore] -> ScoreBoard
+dbScoresToScoreBoard scoreEntities =
+  ScoreBoard $
+    Map.fromList
+      [ ((TeamNumber (Db.teamRoundScoreTeamNumber score), RoundNumber (Db.teamRoundScoreRoundNumber score)), Points (Db.teamRoundScorePoints score))
+      | Entity _ score <- scoreEntities
+      ]
+
+listQuizzes :: Pool SqlBackend -> Handler [QuizSummary]
+listQuizzes pool = runDb pool statement
+ where
+  statement = do
+    quizEntities <- selectList [] []
+    pure $ map toQuizSummary quizEntities
+   where
+    toQuizSummary (Entity quizKey quiz) =
+      QuizSummary
+        { quizId = quizKeyToId quizKey
+        , identifier = quizToIdentifier quiz
+        , active = Db.quizActive quiz
+        }
+
+createQuiz :: Pool SqlBackend -> CreateQuizRequest -> Handler (Quiz 'Active)
+createQuiz pool request = do
+  quizKey <- runDb pool statement
   pure $
     Quiz
-    -- todo: The conversion should be extracted.
-      { quizId = QuizId (fromIntegral $ fromSqlKey quizId)
+      { quizId = quizKeyToId quizKey
       , identifier = request.identifier
       , rounds = []
       -- todo: better - take the teams from the insert statement.
@@ -130,17 +181,218 @@ createQuiz pool _user request = do
 
     pure quizId
 
-getQuiz :: AuthenticatedUser -> QuizId -> Handler SomeQuiz
-getQuiz = undefined
+getQuiz :: Pool SqlBackend -> QuizId -> Handler SomeQuiz
+getQuiz pool quizId = do
+  maybeResult <- runDb pool statement
+  case maybeResult of
+    Nothing -> throwError err404
+    Just (quizIdentifier, domainRounds, domainTeams, scoreBoard, isActive) ->
+      let mkQuiz :: Quiz state
+          mkQuiz =
+            Quiz
+              { quizId = quizId
+              , identifier = quizIdentifier
+              , rounds = domainRounds
+              , teams = domainTeams
+              , scoreBoard = scoreBoard
+              }
+       in if isActive
+            then pure $ SomeActive mkQuiz
+            else pure $ SomeLocked mkQuiz
+ where
+  statement = do
+    let dbQuizId = quizIdToKey quizId
+    maybeQuiz <- get dbQuizId
+    case maybeQuiz of
+      Nothing -> pure Nothing
+      Just quiz -> do
+        teamEntities <- selectList [Db.TeamQuizId ==. dbQuizId] []
+        roundEntities <- selectList [Db.RoundQuizId ==. dbQuizId] []
+        scoreEntities <- selectList [Db.TeamRoundScoreQuizId ==. dbQuizId] []
 
-updateSettings :: AuthenticatedUser -> QuizId -> UpdateQuizSettingsRequest -> Handler (Quiz 'Active)
-updateSettings = undefined
+        let domainTeams = map (dbTeamToTeam . entityVal) teamEntities
 
-updateScore :: AuthenticatedUser -> QuizId -> UpdateScoreRequest -> Handler ScoreBoard
-updateScore = undefined
+        let domainRounds = map (dbRoundToRound . entityVal) roundEntities
 
-lockQuiz :: AuthenticatedUser -> QuizId -> Handler (Quiz 'Locked)
-lockQuiz = undefined
+        let scoreBoard = dbScoresToScoreBoard scoreEntities
 
-unlockQuiz :: AuthenticatedUser -> QuizId -> Handler (Quiz 'Active)
-unlockQuiz = undefined
+        let quizIdentifier = quizToIdentifier quiz
+
+        pure $ Just (quizIdentifier, domainRounds, domainTeams, scoreBoard, Db.quizActive quiz)
+
+updateSettings :: Pool SqlBackend -> QuizId -> UpdateQuizSettingsRequest -> Handler (Quiz 'Active)
+updateSettings pool quizId request = do
+  result <- runDb pool statement
+  case result of
+    Nothing           -> throwError err404
+    Just (Left ())    -> throwError err409
+    Just (Right quiz) -> pure quiz
+ where
+  statement = do
+    let dbQuizId = quizIdToKey quizId
+    maybeQuiz <- get dbQuizId
+    case maybeQuiz of
+      Nothing -> pure Nothing
+      Just quiz
+        | not (Db.quizActive quiz) -> pure $ Just (Left ())
+        | otherwise -> do
+            -- Delete existing rounds and recreate
+            existingRounds <- selectList [Db.RoundQuizId ==. dbQuizId] []
+            forM_ existingRounds $ \(Entity _ _) -> pure () -- Rounds have composite PK, handle separately
+
+            -- Insert new rounds based on questionsPerRound
+            forM_ (zip [1 ..] (questionsPerRound request.settings)) $ \(roundNum, numberOfQuestions) -> do
+              upsert
+                Db.Round
+                  { roundQuizId = dbQuizId
+                  , roundRoundNumber = roundNum
+                  , roundReachablePoints = 0 -- Default, can be updated later
+                  , roundNumberOfQuestions = fromIntegral $ unNumberOfQuestions numberOfQuestions
+                  }
+                [Db.RoundNumberOfQuestions =. fromIntegral (unNumberOfQuestions numberOfQuestions)]
+
+            -- Adjust team count
+            existingTeams <- selectList [Db.TeamQuizId ==. dbQuizId] []
+            let currentTeamCount = length existingTeams
+            let targetTeamCount = numberOfTeams request.settings
+
+            -- Add new teams if needed
+            forM_ [currentTeamCount + 1 .. targetTeamCount] $ \teamNumber -> do
+              insert $
+                Db.Team
+                  { teamQuizId = dbQuizId
+                  , teamNumber = teamNumber
+                  , teamName = ""
+                  , teamActive = True
+                  }
+
+            -- Fetch updated data
+            teamEntities <- selectList [Db.TeamQuizId ==. dbQuizId] []
+            roundEntities <- selectList [Db.RoundQuizId ==. dbQuizId] []
+            scoreEntities <- selectList [Db.TeamRoundScoreQuizId ==. dbQuizId] []
+
+            let domainTeams = map (dbTeamToTeam . entityVal) teamEntities
+
+            let domainRounds = map (dbRoundToRound . entityVal) roundEntities
+
+            let scoreBoard = dbScoresToScoreBoard scoreEntities
+
+            pure $
+              Just $
+                Right
+                  Quiz
+                    { quizId = quizId
+                    , identifier = quizToIdentifier quiz
+                    , rounds = domainRounds
+                    , teams = domainTeams
+                    , scoreBoard = scoreBoard
+                    }
+
+updateScore :: Pool SqlBackend -> QuizId -> UpdateScoreRequest -> Handler ScoreBoard
+updateScore pool quizId request = do
+  result <- runDb pool statement
+  case result of
+    Nothing            -> throwError err404
+    Just (Left ())     -> throwError err409
+    Just (Right board) -> pure board
+ where
+  statement = do
+    let dbQuizId = quizIdToKey quizId
+    maybeQuiz <- get dbQuizId
+    case maybeQuiz of
+      Nothing -> pure Nothing
+      Just quiz
+        | not (Db.quizActive quiz) -> pure $ Just (Left ())
+        | otherwise -> do
+            -- Upsert the score
+            _ <-
+              upsert
+                Db.TeamRoundScore
+                  { teamRoundScoreQuizId = dbQuizId
+                  , teamRoundScoreTeamNumber = unTeamNumber request.teamNumber
+                  , teamRoundScoreRoundNumber = unRoundNumber request.roundNumber
+                  , teamRoundScorePoints = unPoints request.points
+                  }
+                [Db.TeamRoundScorePoints =. unPoints request.points]
+
+            -- Fetch updated scoreboard
+            scoreEntities <- selectList [Db.TeamRoundScoreQuizId ==. dbQuizId] []
+            let board =
+                  ScoreBoard $
+                    Map.fromList
+                      [ ((TeamNumber (Db.teamRoundScoreTeamNumber score), RoundNumber (Db.teamRoundScoreRoundNumber score)), Points (Db.teamRoundScorePoints score))
+                      | Entity _ score <- scoreEntities
+                      ]
+            pure $ Just (Right board)
+
+lockQuiz :: Pool SqlBackend -> QuizId -> Handler (Quiz 'Locked)
+lockQuiz pool quizId = do
+  maybeQuiz <- runDb pool statement
+  case maybeQuiz of
+    Nothing   -> throwError err404
+    Just quiz -> pure quiz
+ where
+  statement = do
+    let dbQuizId = quizIdToKey quizId
+    maybeQuiz <- get dbQuizId
+    case maybeQuiz of
+      Nothing -> pure Nothing
+      Just quiz -> do
+        update dbQuizId [Db.QuizActive =. False]
+
+        teamEntities <- selectList [Db.TeamQuizId ==. dbQuizId] []
+        roundEntities <- selectList [Db.RoundQuizId ==. dbQuizId] []
+        scoreEntities <- selectList [Db.TeamRoundScoreQuizId ==. dbQuizId] []
+
+        let domainTeams = map (dbTeamToTeam . entityVal) teamEntities
+
+        let domainRounds = map (dbRoundToRound . entityVal) roundEntities
+
+        let scoreBoard = dbScoresToScoreBoard scoreEntities
+
+        pure $
+          Just
+            Quiz
+              { quizId = quizId
+              , identifier = quizToIdentifier quiz
+              , rounds = domainRounds
+              , teams = domainTeams
+              , scoreBoard = scoreBoard
+              }
+
+unlockQuiz :: Pool SqlBackend -> AuthenticatedUser -> QuizId -> Handler (Quiz 'Active)
+unlockQuiz pool user quizId = do
+  unless user.isAdmin $
+    throwError err403
+  maybeQuiz <- runDb pool statement
+  case maybeQuiz of
+    Nothing   -> throwError err404
+    Just quiz -> pure quiz
+ where
+  statement = do
+    let dbQuizId = quizIdToKey quizId
+    maybeQuiz <- get dbQuizId
+    case maybeQuiz of
+      Nothing -> pure Nothing
+      Just quiz -> do
+        update dbQuizId [Db.QuizActive =. True]
+
+        teamEntities <- selectList [Db.TeamQuizId ==. dbQuizId] []
+        roundEntities <- selectList [Db.RoundQuizId ==. dbQuizId] []
+        scoreEntities <- selectList [Db.TeamRoundScoreQuizId ==. dbQuizId] []
+
+        let domainTeams = map (dbTeamToTeam . entityVal) teamEntities
+
+        let domainRounds = map (dbRoundToRound . entityVal) roundEntities
+
+        let scoreBoard = dbScoresToScoreBoard scoreEntities
+
+        pure $
+          Just
+            Quiz
+              { quizId = quizId
+              , identifier = quizToIdentifier quiz
+              , rounds = domainRounds
+              , teams = domainTeams
+              , scoreBoard = scoreBoard
+              }
--- src/Api/BackOffice/Types.hs
+++ src/Api/BackOffice/Types.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
+
 module Api.BackOffice.Types where
 
-import Core.Domain (QuizIdentifier)
+import           Core.Domain  (QuizId, QuizIdentifier)
+import           Data.Aeson   (ToJSON)
+import           GHC.Generics (Generic)
 
 data QuizSummary = QuizSummary
-  { identifier :: QuizIdentifier,
-    teamCount :: Int,
-    roundCount :: Int
+  { quizId     :: QuizId
+  , identifier :: QuizIdentifier
+  , active     :: Bool
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic, ToJSON)
